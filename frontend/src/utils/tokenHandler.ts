5805caf891c5d3f5519b22991677452a9ad4b054
Elias Bennett <eliasthebennett@gmail.com>
2025-03-18 14:01:34 +0000

    Modify getAccessTokenFromURL function to save the token to localStorage in tokenHandler.ts. Also add a function to retireve the token from localStorage.

--- frontend/src/utils/tokenHandler.ts
+++ frontend/src/utils/tokenHandler.ts
@@ -1,4 +1,14 @@
 export const getAccessTokenFromURL = (): string | null => {
-    const hashParams = new URLSearchParams(window.location.hash.substring(1));
-    return hashParams.get("access_token");
+  const hashParams = new URLSearchParams(window.location.hash.substring(1));
+  const token = hashParams.get("access_token");
+
+  if (token) {
+    localStorage.setItem("spotify_token", token);
+  }
+
+  return token;
+};
+
+export const getStoredAccessToken = (): string | null => {
+  return localStorage.getItem("spotify_token");
 };
